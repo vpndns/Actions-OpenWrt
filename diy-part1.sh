226ba4932ef477c55e69a180260edc2faafe5859
vpndns <71969787+vpndns@users.noreply.github.com>
2020-10-30 21:02:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,3 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/kenzok8/openwrt-pa...  package/openwrt-packages
 git clone https://github.com/kenzok8/small.git  package/openwrt-small
-git clone https://github.com/project-openwrt/luci-app-koolproxyR
-src-git helloworld https://github.com/fw876/helloworld
