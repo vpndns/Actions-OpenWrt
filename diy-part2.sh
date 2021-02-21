6426e632a376340f950a26f77bcca7974cb8f74d
vpndns <71969787+vpndns@users.noreply.github.com>
2021-02-21 13:40:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
+src-git helloworld https://github.com/fw876/helloworld
+src-git kenzok https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+git clone https://github.com/project-openwrt/luci-app-koolproxyR
+git clone https://github.com/destan19/OpenAppFilter
